f1053bbacdfe3fa6a0edc366a7664ac6f07631d2
Mohammed Hamdoon <Mhamdoon4@Gmail.com>
2023-04-15 00:30:10 +0300

    Reservation working but modal opening needs work

--- client/omwAPP/src/components/reserve/Reserve.jsx
+++ client/omwAPP/src/components/reserve/Reserve.jsx
@@ -1,18 +1,17 @@
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../../hooks/useFetch";
 import { useContext, useState } from "react";
 import { SearchContext } from "../../context/SearchContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const Reserve = (props) => {
+
+const Reserve = ({ setOpen, hotelid }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
   const baseURL = import.meta.env.VITE_REACT_API_URL;
-  const { data, loading, error, reFetch } = useFetch(
-    baseURL + `/hotels/room/${props.hotelid}`
-  );
+  const { data, loading, error } = useFetch(baseURL + `/hotels/room/${hotelid}`);
   const { dates } = useContext(SearchContext);
-  const navigate = useNavigate();
+
   const getDatesInRange = (startDate, endDate) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
@@ -29,15 +28,16 @@ const Reserve = (props) => {
     return dates;
   };
 
-  const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
 
   const isAvailable = (roomNumber) => {
-    const isFound = roomNumber.unavailableDates.some((date) => {
-      allDates.includes(new Date(date).getTime());
-    });
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+
     return !isFound;
   };
-  
+
   const handleSelect = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
@@ -48,42 +48,38 @@ const Reserve = (props) => {
     );
   };
 
+  const navigate = useNavigate();
+
   const handleClick = async () => {
     try {
       await Promise.all(
         selectedRooms.map((roomId) => {
-          const res = axios.put(baseURL + `/rooms/availability/${roomId}`, {
-            dates: allDates,
+          const res = axios.put(baseURL +`/rooms/availability/${roomId}`, {
+            dates: alldates,
           });
-          console.log(roomId);
           return res.data;
         })
       );
-    } catch (err) {
-      console.log(err);
-    }
+      setOpen(false);
+      navigate("/");
+    } catch (err) {}
   };
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Reserve a room
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <span>Select your rooms</span>
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your rooms:</span>
         {data.map((item) => (
-          <div className="rItem">
+          <div className="rItem" key={item._id}>
             <div className="rItemInfo">
               <div className="rTitle">{item.title}</div>
               <div className="rDesc">{item.desc}</div>
               <div className="rMax">
-                Max People: <b>{item.maxPeople}</b>
+                Max people: <b>{item.maxPeople}</b>
               </div>
               <div className="rPrice">{item.price}</div>
             </div>
@@ -102,13 +98,11 @@ const Reserve = (props) => {
             </div>
           </div>
         ))}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" onClick={handleClick} className="rButton">
+        <button onClick={handleClick} className="rButton">
           Reserve Now!
-        </Button>
-      </Modal.Footer>
-    </Modal>
+        </button>
+      </div>
+    </div>
   );
 };
 
